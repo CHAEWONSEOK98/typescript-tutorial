3b3a1574a3c55a42655e350ba18c6f2c5f320b04
CHAEWONSEOK98 <7dugod7@naver.com>
2023-03-16 05:36:45 +0900

    react-ts-tutorial

---
+++ vite-react-ts-tutorial/src/App.tsx
@@ -0,0 +1,24 @@
+import Heading from './components/Heading';
+import { Section } from './components/Section';
+import Counter from './components/Counter';
+import List from './components/List';
+
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState<number>(1);
+
+  return (
+    <>
+      <Heading title={'hello'} />
+      <Section title={'Different Title'}>This is my Section</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={['🍜 ramen', '🍣 sushi', '☕ coffee']}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
+    </>
+  );
+}
+
+export default App;
